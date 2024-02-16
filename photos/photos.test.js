c2796376100d454143550c0f6bb55d6a3e6eaf2b
Alexander Guyer <guyera@oregonstate.edu>
2024-02-15 22:25:58 -0800

    Add code from 2/12 lecture

--- photos/photos.test.js
+++ photos/photos.test.js
@@ -52,3 +52,126 @@ test("a photo card is added when the button is clicked with appropriate inputs",
 	
 	expect(domTesting.queryByText(photoCards[0], "That's a tasty apple pie!")).not.toBeNull()
 })
+
+test("the URL input field is cleared when the button is clicked with appropriate inputs", async function() {
+	// Arrange:
+	initDomFromFiles(`${__dirname}/photos.html`, `${__dirname}/photos.js`)
+	
+	// Acquire the two input fields
+	const urlInput = domTesting.getByLabelText(document, "Photo URL")
+	const captionInput = domTesting.getByLabelText(document, "Caption")
+	
+	// Acquire the button
+	const addPhotoButton = domTesting.getByRole(document, "button")
+
+	// Acquire the list of photo cards (currently empty)
+	const photoCardList = domTesting.getByRole(document, "list")
+
+	// Act:
+	const user = userEvent.setup()
+	await user.type(urlInput, "https://hips.hearstapps.com/hmg-prod/images/apple-pie-index-6425bd0363f16.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*")
+	await user.type(captionInput, "That's a tasty apple pie!")
+	await user.click(addPhotoButton)
+
+	// Assert:
+	// Assert that the URL input field does not have a value
+	expect(urlInput).not.toHaveValue()
+})
+
+test("the caption input field is cleared when the button is clicked with appropriate inputs", async function() {
+	// Arrange:
+	initDomFromFiles(`${__dirname}/photos.html`, `${__dirname}/photos.js`)
+	
+	// Acquire the two input fields
+	const urlInput = domTesting.getByLabelText(document, "Photo URL")
+	const captionInput = domTesting.getByLabelText(document, "Caption")
+	
+	// Acquire the button
+	const addPhotoButton = domTesting.getByRole(document, "button")
+
+	// Acquire the list of photo cards (currently empty)
+	const photoCardList = domTesting.getByRole(document, "list")
+
+	// Act:
+	const user = userEvent.setup()
+	await user.type(urlInput, "https://hips.hearstapps.com/hmg-prod/images/apple-pie-index-6425bd0363f16.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*")
+	await user.type(captionInput, "That's a tasty apple pie!")
+	await user.click(addPhotoButton)
+
+	// Assert:
+	// Assert that the URL input field does not have a value
+	expect(captionInput).not.toHaveValue()
+})
+
+test("the caption input field is NOT cleared when the button is clicked with NO URL", async function() {
+	// Arrange:
+	initDomFromFiles(`${__dirname}/photos.html`, `${__dirname}/photos.js`)
+	
+	// Acquire the two input fields
+	const captionInput = domTesting.getByLabelText(document, "Caption")
+	
+	// Acquire the button
+	const addPhotoButton = domTesting.getByRole(document, "button")
+
+	// Act:
+	const user = userEvent.setup()
+	await user.type(captionInput, "Lonely caption")
+	await user.click(addPhotoButton)
+
+	// Assert:
+	// Assert that the URL input field has value "Lonely caption"
+	expect(captionInput).toHaveValue("Lonely caption")
+})
+
+test("the photo card list remains empty when the button is clicked with NO URL", async function() {
+	// Arrange:
+	initDomFromFiles(`${__dirname}/photos.html`, `${__dirname}/photos.js`)
+	
+	// Acquire the two input fields
+	const captionInput = domTesting.getByLabelText(document, "Caption")
+	
+	// Acquire the button
+	const addPhotoButton = domTesting.getByRole(document, "button")
+
+	// Acquire the list of photo cards (currently empty)
+	const photoCardList = domTesting.getByRole(document, "list")
+
+	// Act:
+	const user = userEvent.setup()
+	await user.type(captionInput, "Lonely caption")
+	await user.click(addPhotoButton)
+
+	// Assert:
+	// Assert that the photo card list is still empty
+	expect(photoCardList).toBeEmptyDOMElement()
+})
+
+
+test("all interactions work on photoCardList as expected when adding multiple photos to the page", async function() {
+	// Arrange:
+	initDomFromFiles(`${__dirname}/photos.html`, `${__dirname}/photos.js`)
+	
+	// Acquire the two input fields
+	const urlInput = domTesting.getByLabelText(document, "Photo URL")
+	const captionInput = domTesting.getByLabelText(document, "Caption")
+	
+	// Acquire the button
+	const addPhotoButton = domTesting.getByRole(document, "button")
+
+	// Acquire the list of photo cards (currently empty)
+	const photoCardList = domTesting.getByRole(document, "list")
+
+	// Act:
+	const user = userEvent.setup()
+	await user.type(urlInput, "https://hips.hearstapps.com/hmg-prod/images/apple-pie-index-6425bd0363f16.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*")
+	await user.type(captionInput, "That's a tasty apple pie!")
+	await user.click(addPhotoButton)
+	await user.type(urlInput, "https://i.redd.it/a604rad7j4651.png")
+	await user.type(captionInput, "MAN")
+	await user.click(addPhotoButton)
+
+	// Assert:
+	// Snapshot testing: Assert that photoCardList matches some
+	// pre-saved snapshot
+	expect(photoCardList).toMatchSnapshot()
+})
